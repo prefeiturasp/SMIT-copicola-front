7da0d2fa18ee4eae0c59afca0bfaeceb3187b0fe
Vinicius Teixeira <viniciusmqueiroga@gmail.com>
2019-09-04 16:32:59 -0300

    Implemented notfound page and adaptative navbar

--- src/router.js
+++ src/router.js
@@ -8,6 +8,11 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
+      path: '*',
+      name: 'NotFound',
+      component: () => import('./views/NotFound.vue')
+    },
+    {
       path: '/',
       name: 'home',
       component: () => import('./views/Home.vue')
